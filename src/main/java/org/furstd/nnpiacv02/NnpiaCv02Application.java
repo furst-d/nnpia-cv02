9ff070ec2f82548b41ba1b35f685eb0d0a4205a8
Dominik Fűrst <dominikfurst1@gmail.com>
2024-02-20 14:12:37 +0100

    Init

---
+++ src/main/java/org/furstd/nnpiacv02/NnpiaCv02Application.java
@@ -0,0 +1,13 @@
+package org.furstd.nnpiacv02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaCv02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnpiaCv02Application.class, args);
+    }
+
+}
