4b2c572cc06b99e4e2211df7a7c742dc4d6bc7d2
Dominik Fűrst <dominikfurst1@gmail.com>
2024-03-07 14:33:48 +0100

    Added bean annotation

--- src/main/java/org/furstd/nnpiacv02/security/configuration/SecurityConfiguration.java
+++ src/main/java/org/furstd/nnpiacv02/security/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package org.furstd.nnpiacv02.security.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,7 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationprovider;
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         final String BASE_PATH = "/api/v1/";
         http
